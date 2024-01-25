52743cf8f5b41c08c1ca78830101514785349562
cheolsoo kim <nabiro@gmail.com>
2024-01-25 17:38:04 +0900

    static 키워드 설명
    public static void printMessage(String message) 처럼 static 키워드를 사용하면 이
    메서드를 클래스의 인스턴스를 생성하지 않고도 직접 호출할 수 있게 하기 위함입니다.
    만약 `static` 키워드를 사용하지 않으면, 메서드를 호출하려면 먼저 클래스의 인스턴스를 생성해야 합니다.

---
+++ sp5-chap02/src/main/java/chap02/CskimMain1.java
@@ -0,0 +1,19 @@
+package chap02;
+
+public class CskimMain1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		// Assign the value "김철수" to the strA variable
+		String varA = "김철수";
+		
+		// Call the printMessage method and pass the strA variable as an argument 
+		printMessage(varA);
+	}
+	
+	// Define another method that takes a string as an argument and prints it 
+	public static void printMessage(String message) {
+		System.out.println("입력된 문자열: " + message);
+	}
+}
